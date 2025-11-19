d81be565aeab2d675f07510c14f252dbc4e6ea6d
Emad <Emad.Salamat@quartech.com>
2025-11-19 11:42:52 -0800

    updated the "CourtRoom" LookupType Display Color

--- db/Migrations/20251119193944_changeCourtRoomDisplayColor.Designer.cs
+++ db/Migrations/20251119193944_changeCourtRoomDisplayColor.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using CAS.DB.models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CAS.DB.Migrations
 {
     [DbContext(typeof(CourtAdminDbContext))]
-    partial class CourtAdminDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251119193944_changeCourtRoomDisplayColor")]
+    partial class changeCourtRoomDisplayColor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ db/Migrations/20251119193944_changeCourtRoomDisplayColor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAS.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeCourtRoomDisplayColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LookupType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DisplayColor",
+                value: "#6C3BAA");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LookupType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DisplayColor",
+                value: "#ffb007");
+        }
+    }
+}
